11426eea9943fcf17057f88013e69d6af3c8b8b5
Augusto Neto <augusto.soliveiran@gmail.com>
2018-09-19 16:38:04 +0000

    Add service

---
+++ src/main/java/com/augustoneto/landon/business/service/ReservationService.java
@@ -0,0 +1,65 @@
+package com.augustoneto.landon.business.service;
+
+import com.augustoneto.landon.business.domain.RoomReservation;
+import com.augustoneto.landon.data.entity.Guest;
+import com.augustoneto.landon.data.entity.Reservation;
+import com.augustoneto.landon.data.entity.Room;
+import com.augustoneto.landon.data.repository.GuestRepository;
+import com.augustoneto.landon.data.repository.ReservationRepository;
+import com.augustoneto.landon.data.repository.RoomRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class ReservationService {
+    private RoomRepository roomRepository;
+    private GuestRepository guestRepository;
+    private ReservationRepository reservationRepository;
+
+    @Autowired
+    public ReservationService(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
+        this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
+    }
+
+    public List<RoomReservation> getRoomReservationsForDate(Date date){
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        Map<Long, RoomReservation> roomReservationMap = new HashMap<>();
+        rooms.forEach(room->{
+            RoomReservation roomReservation = new RoomReservation();
+            roomReservation.setRoomId(room.getId());
+            roomReservation.setRoomName(room.getName());
+            roomReservation.setRoomNumber(room.getNumber());
+            roomReservationMap.put(room.getId(), roomReservation);
+        });
+        Iterable<Reservation> reservations = this.reservationRepository.findByDate(new java.sql.Date(date.getTime()));
+        if(null!=reservations){
+            reservations.forEach(reservation -> {
+                Optional<Guest> guestResponse = this.guestRepository.findById(reservation.getGuestId());
+                if(guestResponse.isPresent()){
+                    Guest guest = guestResponse.get()
+                    RoomReservation roomReservation = roomReservationMap.get(reservation.getId());
+                    roomReservation.setDate(date);
+                    roomReservation.setFirstName(guest.getFirstName());
+                    roomReservation.setLastName(guest.getLastName());
+                    roomReservation.setGuestId(guest.getId());
+                }
+            });
+        }
+        List<RoomReservation> roomReservations = new ArrayList<>();
+        for(Long roomId:roomReservationMap.keySet()){
+            roomReservations.add(roomReservationMap.get(roomId));
+        }
+        return roomReservations;
+    }
+}
+
